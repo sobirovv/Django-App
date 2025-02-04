78e3d4d59d69cc2bf17c6d67c634eb1b75df64ad
sobirovv <dilshodsobirov@Dilshods-MacBook-Pro.local>
2025-02-04 09:24:21 +0100

    Added Templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [                                   #blog.views we created dummy data
+    {                                       #which is list of dictionaries has 2 dummy posts
+        'author': 'Gaddar',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2025'
+    },
+    {
+        'author': 'Mesut Dandun',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2024'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
